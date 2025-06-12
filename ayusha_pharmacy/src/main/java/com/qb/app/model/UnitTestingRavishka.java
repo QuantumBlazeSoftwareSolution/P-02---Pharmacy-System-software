d5f0181b03086aa4af014669a75d0ac2544bb64a
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-12 13:18:51 +0530

    Update UnitTestingRavishka.java

--- ayusha_pharmacy/src/main/java/com/qb/app/model/UnitTestingRavishka.java
+++ ayusha_pharmacy/src/main/java/com/qb/app/model/UnitTestingRavishka.java
@@ -1,10 +1,22 @@
 
 package com.qb.app.model;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+
+
 
 public class UnitTestingRavishka {
     public static void main(String[] args) {
         System.out.println("Your password is: "+PasswordEncryption.hashPassword("123"));
+        JPATest();
+        
+    }
+     
+    private static void JPATest(){
+        JPATransaction.runInTransaction((em)->{
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+        });
     }
     
 }
